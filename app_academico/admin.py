b56abb45fb45d4bbfa6bf6bfd673d3d1d14db0d2
Lizzie Maradiaga <lg_castrom@unicah.edu>
2021-07-18 13:14:12 -0600

    Creacion de modelos clase y oferta academica, vistas

---
+++ app_academico/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Clase, Asignatura, OfertaAcademica
+
+# Register your models here.
+
+class ClaseAdmin(admin.ModelAdmin):
+    list_display = ('id','asignatura', 'seccion', 'hora', 'dias', 'aula', 'cupos')
+
+class OfertaAdmin(admin.ModelAdmin):
+    list_display = ('id','anio', 'periodo', 'estado')
+
+admin.site.register(Clase, ClaseAdmin)
+admin.site.register(Asignatura)
+admin.site.register(OfertaAcademica, OfertaAdmin)
