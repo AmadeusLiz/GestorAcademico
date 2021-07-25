e6a72fec21ea81e9522e31693b9f15a15b048fb9
Lizzie Maradiaga <71053273+AmadeusLiz@users.noreply.github.com>
2021-07-24 19:33:48 -0600

    Pruebas de modelos

--- app_academico/models.py
+++ app_academico/models.py
@@ -10,32 +10,12 @@ class Asignatura(models.Model):
     def __str__(self):
         return f'{self.nombre}'
 
-class Clase(models.Model):
-    asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
-    seccion = models.CharField(max_length=4)
-    hora = models.TimeField()
-    dias = models.CharField(max_length=25)
-    aula  = models.CharField(max_length=20)
-    cupos = models.SmallIntegerField(default=10)
-
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=30)
     def __str__(self):
-        return f'{self.asignatura} | {self.seccion} '
+            return f'{self.nombre}'
 
-class OfertaAcademica(models.Model):
-    PERIODOS = (
-        ('1', 'I PERIODO'), 
-        ('2', 'II PERIODO'), 
-        ('3', 'III PERIODO'), 
-    )
 
-    YEAR_CHOICES = [] #https://es.stackoverflow.com/questions/93320/usar-solo-el-a%C3%B1o-en-django-model-datefield
-    for r in range(2017, (datetime.now().year+20)):
-        YEAR_CHOICES.append((r,r))
-
-    anio = models.IntegerField(choices=YEAR_CHOICES, default=datetime.now().year)
-    periodo = models.CharField(max_length=1, choices=PERIODOS, default='1')
-    clases = models.ManyToManyField(Clase, blank=True) #MtM no llevan on delete y el nulo no aplica
-    estado = models.BooleanField(default=True)
 
 class Docente(models.Model):
     GENEROS =(
@@ -53,3 +33,35 @@ class Docente(models.Model):
 
     def __str__(self):
         return f'{self.nombre} {self.apellido}'
+
+# class Matricula(models.Model):
+#     alumno = 
+
+class Clase(models.Model):
+    asignatura = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
+    seccion = models.CharField(max_length=4)
+    hora = models.TimeField()
+    dias = models.CharField(max_length=25)
+    aula  = models.CharField(max_length=20)
+    cupos = models.SmallIntegerField(default=10)
+    docente = models.ForeignKey(Docente, on_delete=models.CASCADE, null=True, blank=True)
+    alumnos = models.ManyToManyField(Alumno, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.asignatura} | {self.seccion} '
+        
+class OfertaAcademica(models.Model):
+    PERIODOS = (
+        ('1', 'I PERIODO'), 
+        ('2', 'II PERIODO'), 
+        ('3', 'III PERIODO'), 
+    )
+
+    YEAR_CHOICES = [] #https://es.stackoverflow.com/questions/93320/usar-solo-el-a%C3%B1o-en-django-model-datefield
+    for r in range(2017, (datetime.now().year+20)):
+        YEAR_CHOICES.append((r,r))
+
+    anio = models.IntegerField(choices=YEAR_CHOICES, default=datetime.now().year)
+    periodo = models.CharField(max_length=1, choices=PERIODOS, default='1')
+    clases = models.ManyToManyField(Clase, blank=True) #MtM no llevan on delete y el nulo no aplica
+    estado = models.BooleanField(default=True)
