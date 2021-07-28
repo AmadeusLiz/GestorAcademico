8e97d739ac8c0c4926e82c6b1de2cbbad85c19ea
josue abisai lopez <52940936+JosueAbisaiLopez0930@users.noreply.github.com>
2021-07-28 01:23:54 -0600

    Avances con clases matriculas
    cards

--- app_academico/views.py
+++ app_academico/views.py
@@ -6,6 +6,36 @@ from django.urls import reverse
 def index(request):
     return render(request, 'academico/index.html')
 
+def clasesMatricula(request):
+    asignaturas = Asignatura.objects.all().order_by('nombre')
+
+    if request.method == 'POST':
+        asignatura = get_object_or_404(Asignatura, pk=request.POST.get('asignatura'))
+        seccion = request.POST.get('seccion')
+        hora = request.POST.get('hora')
+        dias = request.POST.get('dias')
+        aula = request.POST.get('aula')
+        
+
+        Clase.objects.filter(pk=id).update(asignatura=asignatura, seccion=seccion, hora=hora, dias=dias, aula=aula)
+
+
+    q = request.GET.get('q')
+
+    if q:
+        clases = Clase.objects.filter(asignatura__nombre__contains=q).order_by(
+            'asignatura')  # asignatura es unicamente un id, para acceder al nombre de la asignatura se usa doble guion bajo
+    else:
+        clases = Clase.objects.all().order_by('asignatura')
+
+    ctx = {
+        'activo': 'clases',
+        'clases': clases,
+        'asignaturas': asignaturas,
+    }
+
+    return render(request, 'academico/clasesMatriculadas.html', ctx)
+
 def alumnos(request):
     if request.method=='POST':
         nombre  = request.POST.get('nombre')
